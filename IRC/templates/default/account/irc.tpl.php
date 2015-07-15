3c19abb71c157727832b66ee22aa9ff73242acba
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-15 20:46:15 +0100

    Adding nickserv password

--- IRC/templates/default/account/irc.tpl.php
+++ IRC/templates/default/account/irc.tpl.php
@@ -97,14 +97,15 @@
 
 
     			<div class="social">
-
+			    <h2>Add a new channel:</h2>
     			    <p>
     			    <form
     				action="<?= \Idno\Core\site()->config()->getDisplayURL() ?>account/irc/"
     				class="form-horizontal" method="post">
-    				<input type="text" name="server" placeholder="Network, e.g. irc.freenode.net" />
-    				<input type="text" name="channel" placeholder="Channel, e.g. #knownchat" />
-    				<input type="text" name="username" placeholder="Username to use" />
+    				<input type="text" name="server" placeholder="Network & port, e.g. irc.freenode.net:6697" required />
+    				<input type="text" name="channel" placeholder="Channel, e.g. #knownchat" required />
+				<input type="text" name="username" placeholder="Username to use" required />
+				<input type="password" name="password" placeholder="nickserv password (optional)" required />
     				<input type="submit" value="Add channel..." class="btn btn-default" />
 				<?= \Idno\Core\site()->actions()->signForm('/account/irc/') ?>
     			    </form>
