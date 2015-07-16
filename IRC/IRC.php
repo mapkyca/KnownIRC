7b8b80726df76609e0f1fbaf09bfa47c1941b97e
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-16 13:29:32 +0100

    IRC Helper class

--- IRC/IRC.php
+++ IRC/IRC.php
@@ -7,10 +7,41 @@
 	    private $socket;
 	    
 	    protected function send($string) {
-		if ($written = fwrite($this->socket, $string."\r\n")===false)
-			throw new \Exception('There was a problem writing to IRC');
+		if (!$this->socket) {
+		    throw new \Exception("Not connected to IRC Server");
+		}
+		
+		error_log("IRC > $string");
+				
+		$written = fwrite($this->socket, $string."\r\n");
+		if ($written===false)
+			throw new \Exception("There was a problem writing '$string' to IRC");
+		
+		// Give server a chance
+		sleep(2); 
 		
 		return $written;
+	    }
+	    
+	    /**
+	     * Read a line from the IRC server, stripping the :server.domain.com portion
+	     */
+	    protected function readline() {
+		if (!$this->socket) {
+		    throw new \Exception("Not connected to IRC Server");
+		}
+		
+		$string = fgets($this->socket);
+		
+		error_log("IRC < $string");
+		
+		//$result = explode(' ', $string, 2);
+		//return trim($result[1]);
+		return $string;
+	    }
+	    
+	    function privmesg($target, $message) {
+		return $this->send("PRIVMSG $target :$message");
 	    }
 	    
 	    function connect($server, $port = 6697) {
@@ -18,10 +49,28 @@
 		if ($port == 6697)
 		    $prefix = 'tls://';
 		
-		if ($this->socket = fsockopen($prefix.$server, $port, $errno, $errstr)) {
-		    throw new \Exception($errstr);
+		$this->socket = fsockopen($prefix.$server, $port, $errno, $errstr);
+
+		if (!$this->socket) {
+		    throw new \Exception("IRC Socket - $errstr");
 		}
 			
+	    }
+
+	    /**
+	     * Do a PING/PONG
+	     * @param string $string If null, this method will read from the socket until it gets a PING message.
+	     */
+	    function pingpong($string = null) {
+		
+		if (!$string) {
+		    do {
+			$string = $this->readline();
+		    } while (strpos($string, 'PING')!==0);
+		} 
+		
+		$components = explode(':', $string, 2);
+		return $this->send("PONG :{$components[1]}");
 	    }
 	    
 	    function join($channel) {
@@ -32,12 +81,17 @@
 		$this->send("USER $username $username bla :$username");
 		$this->send("NICK $username");
 		if ($password)
-		    $this->send("PRIVMSG NickServ :IDENTIFY $username $password");    
+		    $this->send("PRIVMSG NickServ :IDENTIFY $username $password"); 
+		
 	    }
 	    
 	    function disconnect() {
 		$this->send('QUIT');
-		return fclose($this->socket);
+		$return = fclose($this->socket);
+		$this->socket = null;
+		
+		return $return;
 	    }
+	    
 	}
     }
